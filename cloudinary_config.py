1dfd50fd3002cb09bcdedbe3bd4cafd86c0c7f3c
Prachi-Shende <163986045+Prachi-Shende@users.noreply.github.com>
2025-07-17 03:02:03 +0530

    Remove sensitive files from repo

--- cloudinary_config.py
+++
@@ -1,9 +0,0 @@
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
-
-cloudinary.config(
-  cloud_name = "dh9sczrso",
-  api_key = "393167437651832",
-  api_secret = "4zLVBehN2jfgPZwkI-Kn1UrG8lw"
-)
